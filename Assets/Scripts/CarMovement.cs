6555f469056ca00363c44e1f1e96cec79c10f6f1
SonkaSoka <natrsonia@gmail.com>
2023-05-18 10:48:22 +0200

    car movement
    car go brr brr

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,8 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
+    //public properties
+
+    public float timeAccel;
+
+
+    //Private properties
+    private float xAxis; 
+    private  Rigidbody _rb;
+    public int force;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        InvokeRepeating("Acceleration", timeAccel, timeAccel);
+
+    }
+
+    public void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 forceVector = new Vector3(xAxis * force, 0f, 0f);
+        _rb.AddForce(forceVector);
+
+    }
     
+    public void Acceleration() 
+    {
+
+        force++;
+
+            
+    }
 }
